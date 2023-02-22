6065e4f1b5cfc3309a2cb493674f2927c47f0c33
chlip1 <lukasz.chlipala3@gmail.com>
2023-02-22 17:02:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const config = require('./config')
+const apiRouter = require('./routes/api')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+//db
+require('./db/mongoose');
+
+//parser
+app.use(bodyParser.json());
+
+//fix cors
+app.use(cors());
+
+// routes
+app.use('/api/', apiRouter);
+
+app.listen(config.port, function() {
+    console.log("serwer slucha... ")
+})
